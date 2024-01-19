bc564c9721ffa6e06379b0f269502dad6430a855
aliciasnts <145516409+aliciasnts@users.noreply.github.com>
2024-01-19 13:56:38 -0300

    Create q6.c

---
+++ lista 3/q6.c
@@ -0,0 +1,11 @@
+/*Escrever um programa para ler peso (massa) e altura de uma pessoa, calcular o 
+indice de massa corporal - IMC (massa/altura^2) e determinar o tipo de 
+obesidade da pessoa conforme a tabela da OMS
+
+< 18,5 - magreza
+entre 18,5 E 24,9 - normal
+entre 25,0 E 29,9 - obesidade grau 1
+entre 30,0 E 39,9 - obesidade grau 2
+> 40,0 - obesidade grave (obesidade grau 3)
+.
+*/
