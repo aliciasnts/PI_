cb5ecc79423214b0699ad18b9d180e31329cf952
aliciasnts <145516409+aliciasnts@users.noreply.github.com>
2024-01-20 13:08:06 -0300

    Update q1.c

--- lista 3/q1.c
+++ lista 3/q1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+float aumentoSalario (float a) {
+    if (a>850) {
+        printf("Aumento não disponivel");
+        return 0; }
+    else {
+        printf("seu novo salário é %.2f\n", a*(1.5));
+        return;}
+}
+int main() {
+  
+    int salarioUsuario;
+    
+    printf("Qual o seu salário?");
+    scanf("%d", &salarioUsuario);
+    
+    return aumentoSalario(salarioUsuario);
+
+}
