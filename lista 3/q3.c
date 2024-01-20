8b4645131ae106bfbbb0afaf8ec114dc8abb8af7
aliciasnts <145516409+aliciasnts@users.noreply.github.com>
2024-01-20 20:48:10 -0300

    Update q3.c

--- lista 3/q3.c
+++ lista 3/q3.c
@@ -7,3 +7,43 @@ APROVADO – se média igual ou superior a 7;
 EM RECUPERAÇÃO – se não aprovado e com
 média igual ou superior a 5; REPROVADO –
 quando média inferior a 5.*/
+
+#include <stdio.h>
+
+float mediapond (float a, float b, float c) {
+    float pesos;
+    pesos = a + (b*2) + (c*3);
+    float media;
+    media = pesos/6;
+    
+    if (media>=7) {
+        printf("Aprovado, média:%.1f" , media);
+        return;
+    }
+    else if (media>= 5) {
+        printf ("Em recuperação, média:%.1f", media);
+        return;
+    }
+    else {
+        printf("Reprovado, média:%.1f", media);
+        return;
+    }
+}
+int main() {
+    
+    float nota1;
+    float nota2;
+    float nota3;
+        
+    printf("Insira sua primeira nota: Ex: 9.0\n");
+    scanf("%f", &nota1);
+    printf("Insira sua segunda nota:\n");
+    scanf("%f", &nota2);
+    printf("Insira sua terceira nota:\n");
+    scanf("%f", &nota3);
+    
+    printf("\n");
+    mediapond(nota1, nota2, nota3);
+
+    return; 
+}
