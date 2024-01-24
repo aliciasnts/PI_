31ee05d2f961b700dbadd70c7da7a2da1a827f8e
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-01-24 13:41:45 -0300

    Update q4.c

--- lista 3/q4.c
+++ lista 3/q4.c
@@ -7,3 +7,29 @@ NÚMERO GERADO MAIOR QUE NÚMERO LIDO ou
 NÚMERO LIDO MAIOR QUE NÚMERO GERADO, ou
 ainda, NÚMERO GERADO IGUAL AO NÚMERO
 LIDO.*/
+
+
+#include <stdio.h>
+#include <math.h> 
+
+
+int inverter (int a) {
+    int valorCentena = a/100; //retorna 1
+    int resto = a%100; //sobra 23
+    int valorDezena = resto/10; // retorna 2
+    int valorUnidade = resto%10; 
+    
+    return valorUnidade*100 + valorDezena*10 + valorCentena;
+    
+}
+int main() {
+    
+    int numeroUsuario;
+    
+    printf("Digite um número:");
+    scanf("%d", &numeroUsuario );
+    
+    printf("O número inverso dele é: %d", inverter(numeroUsuario));
+
+    return 0;
+}
