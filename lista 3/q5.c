16a1d68660733b24964b554b794456e2d9ccc7a0
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-01-24 14:12:49 -0300

    Update q5.c

--- lista 3/q5.c
+++ lista 3/q5.c
@@ -6,3 +6,33 @@ dados pelo usuário. Atendendo a condição
 estabelecida, emitir mensagem “ATENDE
 CONDIÇÃO”; caso contrário, exibir: “NÃO
 ATENDE CONDIÇÃO”.*/
+#include <stdio.h>
+#include <math.h> 
+
+
+void dividir (int a, int b, int c) {
+    if ((c % a == 0 && c % b != 0) || (c % a != 0 && c % b == 0)) {
+        printf("ATENDE A CONDIÇÃO");
+         }
+        
+    else {
+        printf("NÃO ATENDE CONDIÇÃO");
+    }
+}
+int main() {
+    
+    int numeroUsuario, valorA, valorB;
+    
+    printf("Digite um número:");
+    scanf("%d", &numeroUsuario );
+    
+   printf("Digite um número A:");
+    scanf("%d", &valorA );
+    
+    printf("Digite um número B:");
+    scanf("%d", &valorB );
+
+    dividir(valorA, valorB, numeroUsuario);
+    
+    return 0;
+}
