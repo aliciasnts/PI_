31abe7aa07dfe803baae70e856eca1e96ad23b55
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-01-29 18:35:52 -0300

    Update q8.c

--- lista 3/q8.c
+++ lista 3/q8.c
@@ -10,3 +10,54 @@ Deve ser aplicado switch-case.
 Deve haver mensagem de erro para o caso do usuário
 selecionar opção não existente.
 Deve ser modularizado.*/
+
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+
+int dezMilhar (int n) {
+    return n / 10000;
+}
+int unidMilhar (int n) {
+    return (n / 1000) % 10;
+}
+int centena (int n) {
+    return (n / 100) % 10;
+}
+int dezena (int n) {
+    return (n / 10) % 10;
+}
+int unidade (int n) {
+    return n%10;
+}
+
+int main() {
+    
+    int numero;
+    char operador;
+    
+    
+    printf("Digite um número inteiro:\n");
+    scanf("%d" , &numero);
+    
+    printf("Selecione qual das opções deseja exibir:\n");
+   
+    printf("U – unidade\n");
+    printf("D – dezena\n");
+    printf("C – centena\n");
+    printf("M – unidade de milhar\n");
+    printf("Z – dezena de milhar\n");
+    
+    scanf(" %c" , &operador);
+    
+    operador = toupper(operador);
+
+    switch (operador) {
+        case 'U': printf ("O número dado tem %d unidades" , unidade(numero)); break;
+        case 'D': printf ("O número dado tem %d dezenas" , dezena(numero)); break;
+        case 'C': printf ("O número dado tem %d centenas" , centena(numero)); break;
+        case 'M': printf ("O número dado tem %d unidades de milhar" , unidMilhar(numero)); break;
+        case 'Z': printf ("O número dado tem %d dezenas de milhar" , dezMilhar(numero)); break;
+    }
+    return 0;
+}
