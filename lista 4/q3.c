e51bfac3a330dbb2d31f456f11d8badc8c73b065
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-01-31 09:52:08 -0300

    Create q3.c

---
+++ lista 4/q3.c
@@ -0,0 +1,8 @@
+/* Ler notas de alunos e identificar se cada foi aprovado.
+Considerar como sistema de avaliação: em princípio, 3
+notas; são aprovados os alunos com média 7; para os
+alunos com média acima de 3, é possibilitado fazer 4a
+avaliação, então a média passa a ser 5 para obtenção da
+aprovação. Após a identificação de que um aluno foi
+aprovado, ou não; deve ser questionado se o usuário (do
+programa) deseja verificar a situação de outro aluno. */
