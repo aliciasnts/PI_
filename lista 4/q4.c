923e06e9fa3f27ca473ed228e5f340ee0f660415
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-01-31 11:06:28 -0300

    Update q4.c

--- lista 4/q4.c
+++ lista 4/q4.c
@@ -1,1 +1,22 @@
+#include <stdio.h> 
 
+int main () {
+    float mes, dias; 
+    
+    printf("Digite a quantidade de dias:");
+    scanf("%f" , &dias);
+    
+    if (dias>30) {
+    
+    while(dias>=30) {
+        dias = dias - 30;
+        mes = mes + 1;
+    }
+    
+    printf("Tem %.0f mêses", mes);
+}
+    else {
+        printf("Não há dias o bastante pra fechar um mês");
+    }
+    
+    return 0;
