325a4ecf4adf93039d2f424df82fd567fc1dc8c4
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-01-31 11:25:09 -0300

    Update q5.c

--- lista 4/q5.c
+++ lista 4/q5.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+void analisePH (float pH) {
+  
+    if (pH < 7) {
+        printf("\nACIDA"); } 
+        
+    else if (pH > 7) {
+    printf("\nBASICA");} 
+    
+    else {
+    printf("\nNEUTRA");} 
+    
+}
+
+int main() {
+  
+  printf("Cole a lista dos PH de todas as substâncias que deseja analizar, digitando -1 no final da lista para parara a execução do programa.\n");
+  
+    float entrada;
+
+ while (1) {
+   scanf("%f", &entrada);
+    if (entrada == -1) {break;}
+ 
+ analisePH(entrada);
+    }
+
+return 0;
+}
