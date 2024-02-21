44b12cc76112484a83b09d7cc7e67535afecea19
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-02-21 13:59:34 -0300

    Delete lista5/q4.c

--- lista5/q4.c
+++
@@ -1,6 +0,0 @@
-/* Escrever programa para ler o nome de uma pessoa,
-composto por nome e sobrenome (entre nome e
-sobrenome há um espaço em branco). E exibir,
-sobrenome em maiúsculo, seguido de vírgula e do nome
-da pessoa. Efetuar essa operação para tantas entradas quantas
-o usuário desejar.*/
