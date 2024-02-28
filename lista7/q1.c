0781a565a7cdecda714cd69e24cd3cb68015f92c
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-02-28 11:36:32 -0300

    Update q1.c

--- lista7/q1.c
+++ lista7/q1.c
@@ -1,1 +1,9 @@
+/*Sabendo-se que Y=toupper(X) corresponde a
+função em C (biblioteca ctype.h) que atribui à
+variável Y do tipo char o valor da variável X
+(também do tipo char) em maiúsculo.
 
+Escrever programa para ler uma palavra com até 20
+caracteres e exibi-la com letras maiúsculas.
+Exemplos: dado brasileiro, retorna BRASILEIRO.
+Dado Nordeste, retorna NORDESTE*/
