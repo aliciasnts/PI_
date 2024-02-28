38773618b7d8c56847e04bab751098ded40d9c60
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-02-28 11:35:40 -0300

    Create q5.c

---
+++ lista 6/q5.c
@@ -0,0 +1,6 @@
+/*(Adaptado do 3o Exercício Resolvido – Cap. 5 – Fund.
+Da Prog. De Computadores) Faça um programa
+para ler N números inteiros e positivos e de cada
+um desses apresente o fatorial. O cálculo do fatorial
+deve ser efetuado por meio de função elaborada
+pelo programador.*/
