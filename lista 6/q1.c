63af5959e09406f32c8c67e135740778fa04bba4
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-02-28 12:09:57 -0300

    Update q1.c

--- lista 6/q1.c
+++ lista 6/q1.c
@@ -6,3 +6,31 @@ peso inicial e o peso almejado, exibir o peso alcançado
 corrigir os erros que ocorrem quando o peso almejado
 não é múltiplo de 5, bem como quando o peso inicial é
 menor que o peso almejado. Aplicar for.*/
+
+#include <stdio.h>
+#include <math.h>
+float calcular( float inicial, float final){
+    if (final>inicial) {
+        printf("Erro: o peso final é maior do que o inicial.");
+    }
+    else{
+        float a = (inicial-final)/5;
+        for (int i=0; i>=a; i++) {
+            (int) a 
+        }
+    }
+    
+}
+
+int main() {
+    
+    float pesoInicial, pesoFinal;
+    
+    printf("Qual o seu peso atual?\n");
+    scanf("%f", &pesoInicial);
+    printf("Qual peso deseja alcançar ao fim da dieta?\n");
+    scanf("%f" , &pesoFinal);
+
+printf("%f", calcular(pesoInicial, pesoFinal));
+    return 0;
+}
