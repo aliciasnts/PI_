115c7fd9c111ad2bbf4bf8331582da9c709ba561
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-02-28 11:33:15 -0300

    Create q4.c

---
+++ lista 6/q4.c
@@ -0,0 +1,5 @@
+/*Na disciplina “Programação Imperativa” há 50 alunos e 12
+avaliações. Para obtenção de aprovação o aluno deve ter no
+mínimo 5 notas maior ou igual a 7. Escrever programa para
+ler notas de cada um dos 50 alunos e identificar se foi
+aprovado.*/
