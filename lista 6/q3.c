0d5c1b18f8584ce95a0b7ad1f0e33c3f7892a05b
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-02-28 11:32:34 -0300

    Create q3.c

---
+++ lista 6/q3.c
@@ -0,0 +1,11 @@
+/*D. Maria tem um ônibus que faz diariamente, das 8 as 18
+horas, o trajeto Rosa Elze – Atalaia. É feita 1 viagem por
+hora e D. Maria registra o número de passageiros de cada
+uma para identificar o horário com maior número de
+passageiros. Para tanto foi implementado e discutido em
+sala um programa em C. Efetuar ajustes nesse de forma
+que seja identificado o horário com menor número de
+passageiros, exibir o número médio de passageiros, a soma
+de passageiros do dia e o montante recebido por D. Maria,
+considerando que a passagem custa R$22,00. Aplicar for e
+pelo menos 1 função.*/
