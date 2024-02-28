8ab00fa8f1cb0ace4a3542503a14fc435cfe4271
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-02-28 11:37:38 -0300

    Create q3.c

---
+++ lista7/q3.c
@@ -0,0 +1,5 @@
+/*Escrever programa para ler uma palavra P qualquer
+com até 20 caracteres e identificar quantas vogais P
+apresenta. Aplicar função definida pelo programador
+Efetuar essa operação para tantas strings quantas o usuário
+desejar.*/
