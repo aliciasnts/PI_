75cb1eb15bd8702513e2c72c997ab51a4a7ff391
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-02-28 11:31:53 -0300

    Create q2.c

---
+++ lista 6/q2.c
@@ -0,0 +1,13 @@
+/*Na loja de Sr. Zé compras acima de R$300,00 podem ser parceladas em
+até 10 vezes, para tanto são aplicados juros simples de 10% ao mês,
+conforme o número de meses. Dado valor da compra, exibir o valor total
+e o valor das parcelas. Aplicar for e função.
+
+Exemplo:
+Compra R$1.000,00
+Pagamento em 1 mês – Juros: R$100,00 – Total: R$1.100 – Parcela1 x R$ 1.100,00
+Pagamento em 2 meses – Juros: R$100,00 – Total – R$1.200 – Parcela 2 x R$600,00
+Pagamento em 3 meses – Juros: R$100,00 – Total – R$1.300 – Parcela 3 x R$433,33
+Pagamento em 4 meses – Juros: R$100,00 –Total – R$1.400 – Parcela 4 x R$350,33
+...
+Pagamento em 10 meses – Juros: R$100,00 –Total – R$2.000 – Parcela 10 x R$200,00*/
