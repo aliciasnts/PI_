9bf640b1a76ec94caebc2197b98fbe8f88a944e1
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-03-09 12:53:34 -0300

    Update q2.c

--- lista7/q2.c
+++ lista7/q2.c
@@ -1,4 +1,6 @@
-/*Escrever programa para ler um nome e, independente
+
+
+/* Escrever programa para ler um nome e, independente
 de como este foi escrito, exibir a inicial maiúscula e o
 restante minúscula. Sendo:
 ● Cadeia[P] = toupper(Cadeia[P]);
@@ -8,4 +10,24 @@ ctype.h.
 ● Cadeia[P] = tolower(Cadeia[P]);
 Converte a letra da posição P da string
 Cadeia para minúscula, da biblioteca
-ctype.h.*/
+ctype.h. */
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main () {
+    char palavra[21];
+  
+    printf ("Insira uma palavra:\n");
+    scanf ("%20s",palavra);
+  
+palavra[0] = toupper(palavra[0]);
+
+    for (int i = 1 ; palavra[i] !='\0'; i++) 
+  
+    palavra[i] = tolower(palavra[i]);
+    
+  
+    printf ("Palavra editada:: %s\n" , palavra);
+  
+}
