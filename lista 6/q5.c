3eb148e16a0ace4737d84fae3078ef88358e0516
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-03-15 23:42:06 -0300

    Update q5.c

--- lista 6/q5.c
+++ lista 6/q5.c
@@ -1,41 +1,46 @@
-/*(Adaptado do 3o Exercício Resolvido – Cap. 5 – Fund.
-Da Prog. De Computadores) Faça um programa
-para ler N números inteiros e positivos e de cada
-um desses apresente o fatorial. O cálculo do fatorial
-deve ser efetuado por meio de função elaborada
-pelo programador.*/
+/* Na universidade “DuSabiTudu” os alunos são identificados por
+matrículas compostas por 10 dígitos. Os 4 primeiros dígitos
+correspondem ao ano em que o aluno entrou na instituição.
+Assim, todos os alunos cujas matriculas iniciam por 2012
+ingressaram no curso superior no ano de 2012. Os dígitos 5 e 6 da
+matrícula correspondem ao curso; sendo: 97 para agroecologia e
+99 para gerontologia. Escrever programa para ler a matricula de
+100 alunos inscritos no programa “CienciasAlémDasFronteiras” e
+identificar quantos alunos ingressaram num dado ano A no curso
+de gerontologia. Aplicar função definida pelo programador. */
 
-#include <stdio.h>
+#include <stdio.h> 
 #include <math.h>
 
-int calcularFatorial(int num) {
-    int fatorial = 1;
+int verificarMatricula(char matricula[], int ano) {
+    // seleciona os dígitos relevantes da matrícula
+    int anoMatricula = (matricula[0] - '0') * 1000 + (matricula[1] - '0') * 100 + (matricula[2] - '0') * 10 + (matricula[3] - '0');
+    int curso = (matricula[4] - '0') * 10 + (matricula[5] - '0');
 
-    // Loop para calcular o fatorial
-    for (int i = 1; i <= num; i++) {
-        fatorial *= i;}
+    // verifica se a matrícula corresponde
+    if (anoMatricula == ano && curso == 99) {
+        return 1;} // matrícula válida
+    else {
+        return 0;}}// matrícula inválida
 
-    return fatorial;}
 
 int main() {
-    int N;
+    char matricula[11];
+    int ano;
+    int alunosGerontologia = 0; //contador de alunos de gerontologia
 
-    // Solicita o número de valores a serem lidos
-    printf("Quantos números inteiros e positivos deseja calcular?");
-    scanf("%d", &N);
+    printf("Digite o ano que deseja checar: ");
+    scanf("%d", &ano);
+    printf("Digite as matriculas dos 100 alunos:\n");
+    
+    for (int i = 0; i < 100; i++) {
+        printf("Matricula do aluno %d: ", i + 1);
+        scanf("%s", matricula);
 
-    // Loop para ler e calcular o fatorial de cada número
-    for (int ind = 0; ind < N; ind++) {
-        int num;
-        printf("Digite o número %d: ", ind + 1);
-        scanf("%d", &num);
 
-        // Verifica se o número é positivo
-        if (num < 0) {
-            printf("O número deve ser positivo.\n");
-        } else {
-            // Calcula o fatorial usando a função personalizada
-            int fatorial = calcularFatorial(num);
-           printf("O fatorial de %d é: %d\n", num, fatorial);}}
+        if (verificarMatricula(matricula, ano)) {
+            alunosGerontologia++;}}
+
+    printf("No ano %d, ingressaram %d alunos no curso de gerontologia", ano, alunosGerontologia);
 
     return 0;}
