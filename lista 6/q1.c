0945cb8052b886a72f45cd304564b0f1653d5bc4
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-03-15 23:40:22 -0300

    Update q1.c

--- lista 6/q1.c
+++ lista 6/q1.c
@@ -1,38 +1,24 @@
-/*Maria está fazendo a “Dieta de Saturno” para
-emagrecimento e a previsão é de que a perda de peso
-seja de 5 kg mensais. Ajustar programa em C para ler o
-peso inicial e o peso almejado, exibir o peso alcançado
-(em teoria) a cada mês, conforme o que prevê a dieta, e
-corrigir os erros que ocorrem quando o peso almejado
-não é múltiplo de 5, bem como quando o peso inicial é
-menor que o peso almejado. Aplicar for.*/
+/*Sabendo-se que Y=toupper(X) corresponde a
+função em C (biblioteca ctype.h) que atribui à
+variável Y do tipo char o valor da variável X
+(também do tipo char) em maiúsculo.
 
+Escrever programa para ler uma palavra com até 20
+caracteres e exibi-la com letras maiúsculas.
+Exemplos: dado brasileiro, retorna BRASILEIRO.
+Dado Nordeste, retorna NORDESTE*/
 #include <stdio.h>
-#include <math.h>
-float calcular( float inicial, float final){
-    int mes;
-    if (final>inicial) {
-        printf("Erro: o peso final é maior do que o inicial.");
-    }
-    else{
-        for (mes=0; inicial>=final; mes++) {
-            printf("No mês %d, o peso atingido foi %1.f \n", mes, inicial);
-            inicial-=5;}}}
-
+#include <ctype.h>
 
 int main() {
-    
-    float pesoInicial, pesoFinal;
-    int mes;
-    
-    printf("Qual o seu peso atual?\n");
-    scanf("%f", &pesoInicial);
-    printf("Qual peso deseja alcançar ao fim da dieta?\n");
-    scanf("%f" , &pesoFinal);
+    char palavra[21]; //contando com o caracter nulo
 
-    calcular(pesoInicial, pesoFinal);
+    printf("Digite uma palavra com até 20 caracteres: ");
+    scanf("%20s", palavra);
+  
+    for(int i = 0; palavra[i] != '\0'; i++) {
+        palavra[i] = toupper(palavra[i]);}
+  
+    printf("Palavra em maiúsculas: %s\n", palavra);
 
-    if (pesoFinal >0 && ((int) pesoFinal % 5) > 0){
-    printf("Peso do mês incompleto = %.1f \n", pesoFinal);}
-
-    return;}
+    return 0;}
