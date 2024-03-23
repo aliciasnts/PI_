4423dd2692e6bc4f09dbb57444a618da9f30b8f1
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-03-22 21:54:55 -0300

    Create q1.c

---
+++ lista 9/q1.c
@@ -0,0 +1,12 @@
+/*Escrever programa em C para ler nome, 3 notas e
+frequência (em percetual) dos 80 alunos do projeto
+AprendaProgramação, calcular a média destes (média da
+turma) e emitir em tela a situação final de cada aluno.
+
+São aprovados todos os que ficam com média (aritmética
+simples) acima da média da turma e têm frequência acima
+de 65%.
+
+Deve ser exibido o nome de cada aluno, seguido das
+notas, média e da situação final: APROVADO ou
+REPROVADO.*/
