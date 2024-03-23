383addb3f50659749b3bee0cdb8e89bb9e1815f8
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-03-22 21:58:49 -0300

    Create q5.c

---
+++ lista 9/q5.c
@@ -0,0 +1,7 @@
+/*Ajustar a principal estrutura de dados usada
+no Projeto Freezers de forma a aplicar structs.
+Selecionar um módulo que manipule os dados
+e reescreva-o considerando a estrutura
+proposta por você. É suficiente apresentar o trecho
+de código (estrutura e módulo). Na
+apresentação/avaliação não é preciso executá-lo.*/
